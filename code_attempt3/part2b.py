eaab3a0fe5aa850ed4efe897acea53efd47c66b7
Erik Osland <eoslan2@uic.edu>
2018-03-04 20:28:26 -0600

    Finally actually doing optimization.  24 second gradient

--- code_attempt3/part2b.py
+++ code_attempt3/part2b.py
@@ -8,25 +8,42 @@ Created on Sat Mar  3 13:02:04 2018
 import gradient_calculation as gc
 import get_data as gd
 from scipy.optimize import fmin_bfgs
+from scipy.optimize import check_grad
 from time import time
+import numpy as np
 import winsound
+
+def func_to_minimize(params, X, y, C):
+    reg = 1/2 * np.sum(params ** 2)
+    avg_prob = gc.avg_log_p_y_given_x(params, X, y, len(X))
+    return -C * avg_prob + reg
+
+def grad_func(parms, X, y, C):
+    grad_avg_prob =  gc.avg_gradient(params, X, y, len(X))
+    grad_reg = params
+    return -C * grad_avg_prob + grad_reg
+    
+
 
 
 params = gd.get_params()
 X, y = gd.read_data_formatted()
 
+
+
+
 start = time()
-out = fmin_bfgs(gc.neg_avg_log_p_y_given_x, params, gc.avg_gradient, (X, y, len(X), 1000), maxiter = 1)
+out = fmin_bfgs(func_to_minimize, params, grad_func, (X, y, 1000), maxiter = 1)
 print("Total time: ", end = '')
 print(time() - start)
 
 
 start = time()
-out = fmin_bfgs(gc.neg_avg_log_p_y_given_x, params, gc.avg_gradient, (X, y, len(X), 1000), maxiter = 2)
+out = fmin_bfgs(func_to_minimize, params, grad_func, (X, y, 1000), maxiter = 2)
 print("Total time: ", end = '')
 print(time() - start)
 
 
 frequency = 2500  # Set Frequency To 2500 Hertz
 duration = 500  # Set Duration To 1000 ms == 1 second
-winsound.Beep(frequency, duration)
+#winsound.Beep(frequency, duration)
--- code_attempt3/sandbox_deleteme.py
+++ code_attempt3/sandbox_deleteme.py
@@ -10,24 +10,11 @@ import get_data as gd
 import numpy as np
 from scipy.optimize import check_grad
 
-''' do this next '''
-def grad_wrt_wy(X, y, w_x, t, f_mess, b_mess, den):
-    gradient = np.zeros(128 * 26)
-    
-    for i in range(len(w_x)):
-        
-            
-    return gradient
+def func(x):
+    return 0.5 *(x[0] * x[0] + x[1] * x[1])
+
+def grad(x):
+    return x
 
 
-X, y = gd.read_data_formatted()
-params = gd.get_params()
-
-
-w = gc.w_matrix(params)
-w_x = np.inner( X[1], w)
-t = gc.t_matrix(params)
-f_mess = gc.forward_propogate(w_x, t)
-b_mess = gc.back_propogate(w_x, t)
-den = gc.denominator(f_mess, w_x)
-
+print(check_grad(func, grad, np.array( [10, 10])))
